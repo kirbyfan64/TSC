e5a1345fae4e8edc101c25d36e0e6468e0fddff1
Marvin Gülker <post+git@guelker.eu>
2019-11-17 15:19:50 +0100

    Add more rigid spacing restrictions to .dir-locals.el.
    Namely, the tab width of 4 spaces should apply everywhere and
    XML files do need 4 space indentation just as all other source files
    as well.

--- tsc/.dir-locals.el
+++ tsc/.dir-locals.el
@@ -2,9 +2,16 @@
 ;;; See Info node `(emacs) Directory Variables' for more information.
 
 ((nil .
-      ((indent-tabs-mode . nil)))
+      ((indent-tabs-mode . nil)
+       (tab-width . 4)
+       (show-trailing-whitespace . t)))
+ (c-mode .
+         ((c-file-style . "k&r")
+          (c-basic-offset . 4)))
  (c++-mode .
-	   ((c-file-style . "stroustrup")
-	    (c-basic-offset . 4)
-	    (tab-width . 4)
-	    (show-trailing-whitespace . t))))
+	       ((c-file-style . "stroustrup")
+	        (c-basic-offset . 4)
+	        (show-trailing-whitespace . t)))
+ (nxml-mode .
+            ((nxml-child-indent . 4)
+             (nxml-attribute-indent . 4))))
